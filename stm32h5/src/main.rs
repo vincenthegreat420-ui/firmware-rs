b264456dfbf8f4f9fad97fbe482d7ceac0073ad8
vincenthegreat420-ui <vincenthegreat420@gmail.com>
2026-06-14 17:33:15 +0400

    Update main.rs

--- stm32h5/src/main.rs
+++ stm32h5/src/main.rs
@@ -239,9 +239,9 @@ async fn main(spawner: Spawner) {
 
     let sai_resources = SaiResources {
         sai: p.SAI1,
-        sck_b: p.PE5,
-        sd_b: p.PE6,
-        fs_b: p.PE4,
+        sck_b: p.PB3,
+        sd_b: p.PB5,
+        fs_b: p.PB4,
         dma_b: p.GPDMA1_CH1,
     };
 
