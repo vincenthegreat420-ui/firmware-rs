6b9b38ace8aed29e7915268f41caefc96356f2b5
vincenthegreat420-ui <vincenthegreat420@gmail.com>
2026-06-14 18:00:33 +0400

    Update main.rs

--- stm32h5/src/main.rs
+++ stm32h5/src/main.rs
@@ -237,13 +237,13 @@ async fn main(spawner: Spawner) {
         cortex_m::peripheral::NVIC::unmask(interrupt::TIM2);
     }
 
-    let sai_resources = SaiResources {
-           sai: p.SAI1,
-        sck_b: p.PE5,
-        sd_b: p.PE6,
-        fs_b: p.PE4,
-        mclk_b: p.PE2,
-        dma_b: p.GPDMA1_CH1,
+       let sai_resources = SaiResources {
+        sai: p.SAI1,
+        sck_a: p.PE5,
+        sd_a: p.PE6,
+        fs_a: p.PE4,
+        mclk_a: p.PE2,
+        dma_a: p.GPDMA1_CH1,
     };
 
     unwrap!(spawner.spawn(blinky_task(
